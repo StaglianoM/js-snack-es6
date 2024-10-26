75e69b77f27e8047390a7357a89d86a00709340f
StaglianoM <stagliano.m92@gmail.com>
2024-10-26 16:31:08 +0200

    football

---
+++ football.js
@@ -0,0 +1,34 @@
+const risultati = [];
+const Squadre = [
+    {
+        name: "Roma",
+        points: 0,
+        foul: 0,
+    },
+    {
+        name: "Lazio",
+        points: 0,
+        foul: 0,
+    },
+    {
+        name: "Boolean",
+        points: 0,
+        foul: 0,
+    },
+];
+
+Squadre.forEach(squadra => {
+    squadra.points = Math.floor(Math.random()*100);
+    squadra.foul = Math.floor(Math.random()*25);
+})
+
+Squadre.forEach(squadra => {
+    const infoSquadra = {
+        name: squadra.name,
+        foul: squadra.foul,
+    };
+    risultati.push(infoSquadra);
+}
+)
+
+console.log(risultati)
